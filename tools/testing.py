268d975cbebafa26313264269bda1e5968a7ce33
georgiagracetully <gtully@stanford.edu>
2025-12-05 15:43:05 -0800

    adding training and testing nbs from sherlock

--- tools/testing.py
+++ tools/testing.py
@@ -2,27 +2,9 @@ import torch
 import pandas as pd
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from rna_datasets import RNA_Dataset
 import os
 
-class RNA_Dataset(Dataset):
-    def __init__(self, data):
-        self.data = data
-        self.tokens = {nt: i for i, nt in enumerate('ACGU')}
-        self.label_names = ['logkd_lig_scaled', 'logkd_nolig_scaled']
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        sequence=[self.tokens[nt] for nt in (self.data.loc[idx,'sequence'])]
-        sequence=np.array(sequence)
-        sequence=torch.tensor(sequence)
-
-        labels = np.array([self.data.loc[idx, l] for l in self.label_names])  # Just 1 value per label
-        labels = torch.tensor(labels, dtype=torch.float32)  # Ensure labels are of correct float type
-
-
-        return {'sequence': sequence, 'labels': labels}
 
 def test_from_checkpoint(checkpoint_path, test_df, model, criterion=None, 
                         batch_size=1, device='cuda', save_predictions=True, 
